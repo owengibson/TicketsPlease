0679589969736d691ca19690a34b073f86a622ce
owengibson <114120616+owengibson@users.noreply.github.com>
2026-04-15 11:40:44 +1000

    chore: create combat and weapon classes
    Codecks IDs: $128 , $129

---
+++ Assets/Scripts/Combat/CombatState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace TP.Combat
+{
+    public enum CombatState
+    {
+        Idle = 0,
+        Attacking = 1,
+        Recovering = 2,
+        Staggered = 3,
+        Disabled = 4
+    }
+}
---
+++ Assets/Scripts/Combat/Weapons/WeaponRuntimeState.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace TP.Combat.Weapons
+{
+    [System.Serializable]
+    public class WeaponRuntimeState
+    {
+
+    }
+}
